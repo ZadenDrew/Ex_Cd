13683c661f8c59ff36be2303a7f74aa1f46188fd
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2018-04-13 09:54:45 +0200

    examen

---
+++ src/main/java/com/mycompany/tiempo_exm/Exm.java
@@ -0,0 +1,19 @@
+package com.mycompany.tiempo_exm;
+
+/**
+ *
+ * @author acabezaslopez
+ */
+public class Exm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tiempo t = new Tiempo();
+        t.titulo();
+        t.grados();
+        t.tiempo();
+    }
+
+}
